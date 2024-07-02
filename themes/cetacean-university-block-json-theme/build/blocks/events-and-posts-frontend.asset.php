57205ae7b9b7e249ce92ecd7255d50b1e7472b60
Vinicius Buchara <vbuchara31@gmail.com>
2024-07-02 18:18:22 -0400

    Removed some legacy files from the Json Block Theme

--- themes/cetacean-university-block-json-theme/build/blocks/events-and-posts-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-primitives'), 'version' => 'b8ae98beee831c8fec51');
